0b79be9918f5eb967ad121ce95376b334bc0e607
Kenneth Maberi <kennethmaberi@gmail.com>
2025-05-09 17:34:24 +0300

    saw

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -2,14 +2,7 @@
 // Dynamically populate footer date information
 
 // 1. Current Year
-const yearSpan = document.getElementById('currentyear');
-if (yearSpan) {
-  const now = new Date();
-  yearSpan.textContent = now.getFullYear();
-}
+document.getElementById("currentyear").textContent = new Date().getFullYear();
 
 // 2. Document Last Modified
-const lastModP = document.getElementById('lastModified');
-if (lastModP) {
-  lastModP.textContent = 'Last Modified: ' + document.lastModified;
-}
+document.getElementById("lastModified").textContent = "Last Modified: " + document.lastModified;
