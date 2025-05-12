9ad4057d98632a52ec176d270fd3fd3c823598ba
Kenneth Maberi <kennethmaberi@gmail.com>
2025-05-12 11:09:42 +0300

    saw

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -3,6 +3,4 @@
 
 // 1. Current Year
 document.getElementById("currentyear").textContent = new Date().getFullYear();
-
-// 2. Document Last Modified
 document.getElementById("lastModified").textContent = "Last Modified: " + document.lastModified;
